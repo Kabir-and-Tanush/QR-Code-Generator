370c19059601c453c280eb6a1e6457a9bd55bdb5
KabrG <kabirguron@gmail.com>
2023-10-10 21:40:50 -0400

    File creation

---
+++ main.py
@@ -0,0 +1,4 @@
+# This is where the main file will be. It will take care of the entire conversion.
+# Ideally, another file can be associated with exporting the image but we can worry about it later
+
+print("hello")
