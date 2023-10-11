256a3154120d2bf1fab684058d3e702f04c99f47
KabrG <kabirguron@gmail.com>
2023-10-10 22:09:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 # This is where the main file will be. It will take care of the entire conversion.
 # Ideally, another file can be associated with exporting the image but we can worry about it later
 
-print("hello")
+import requests
+
+width = 1000
+height = 1000
+
+params={'cht': 'qr',
+        'chs': f'{width}x{height}',
+        'chl': ''}
+r = requests.get(url='https://chart.googleapis.com/chart?', params=params)
+
+print(r)
